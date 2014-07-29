c8d8b1a89b4199c611696ffaf4ef7513c187101d
1julianor <julianoguedes2010@gmail.com>
2014-07-29 10:45:14 -0300

    comit de criação do form inicio

--- LeitorXML/LeitorXML/FormInicio.cs
+++ LeitorXML/LeitorXML/FormInicio.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace LeitorXML
 {
-    public partial class Form1 : Form
+    public partial class FormInicio : Form
     {
-        public Form1()
+        public FormInicio()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
